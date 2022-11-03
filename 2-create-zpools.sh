a6e91ac39aa178f43802626919b88e85f22ea8ed
Qing Zheng <qzheng@lanl.gov>
2022-11-02 19:48:34 -0600

    update zpool creation

--- 2-create-zpools.sh
+++ 2-create-zpools.sh
@@ -4,13 +4,16 @@ curdir=$(cd `dirname $0` && pwd)
 
 cd $curdir || exit 1
 
+sudo modprobe zfs || exit 1
+sudo lsmod | grep zfs
+
 echo $((16*1024*1024)) | sudo tee /sys/module/zfs/parameters/zfs_max_recordsize
 
 if [ `sudo zpool list -Ho name | wc -l` -ne 0 ]
 then
 	echo "FOUND ONE OR MORE ZPOOLS ONLINE!!"
 	exit 0
-fi	
+fi
 
 pushd zpools
 
