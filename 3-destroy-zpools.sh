17361c311d410d28b971032f0cbb1ba6e5dac2a3
Qing Zheng <qzheng@lanl.gov>
2023-02-10 23:17:25 -0700

    add comments

--- 3-destroy-zpools.sh
+++ 3-destroy-zpools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -xu
 
+#
+# destroy all zpools we currently have. all zpools should be destroyed or exported
+# before low-level drive operations (reboot drives) can happen
+#
 for pl in `sudo zpool list -Ho name`
 do
 	sudo zpool destroy $pl
