dae533174f22a9e49ee368316b05e90681b2071e
Qing Zheng <qzheng@lanl.gov>
2023-02-13 13:52:06 -0700

    mount meta pool readonly

--- mount_meta_pool.sh
+++ mount_meta_pool.sh
@@ -4,6 +4,6 @@ sudo nvme connect-all -t tcp -a 192.168.1.251 -s 4420 || exit 1
 
 sleep 3
 
-sudo mount /dev/disk/by-id/nvme-uuid.a4094c31-7dfa-451d-8c19-e3d7209c830a /mnt
+sudo mount -oro,noload /dev/disk/by-id/nvme-uuid.a4094c31-7dfa-451d-8c19-e3d7209c830a /mnt
 
 exit 0
