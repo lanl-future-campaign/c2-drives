8527e1276b2bd027ff7029e453bbbfd5f1eb1289
Qing Zheng <qzheng@lanl.gov>
2023-03-05 13:02:23 -0700

    parallelize ping drives

--- ping_drives.sh
+++ ping_drives.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -u
 
-#
-# say hello to all drives
-#
 curdir=$(cd `dirname $0` && pwd)
 
 cd $curdir || exit 1
@@ -11,10 +8,13 @@ n=$(cat num_drives)
 
 pushd drives
 
-for dr in `ls -d1 */ | cut -d/ -f1 | head -n $n`
-do
-	sudo ssh $dr "hostname -I"
-done
+if true; then
+	sudo pdsh -R ssh -w `ls -1 | grep -v txt | sort | head -n $n | tr '\n' ','` 'hostname -I'
+else
+	for dr in `ls -d1 */ | cut -d/ -f1 | head -n $n`; do
+		sudo ssh $dr "hostname -I"
+	done
+fi
 
 popd
 
